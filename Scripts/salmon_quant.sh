a421160fbca41ac55c8bfbc80b1999541c4e6a1e
Keith Arora-Williams <karoraw1@jhu.edu@login-node02.cm.cluster>
2018-03-30 00:05:08 -0400

    fixed KO abundance pipeline

--- Scripts/salmon_quant.sh
+++ Scripts/salmon_quant.sh
@@ -14,16 +14,17 @@
 #SBATCH --output=sq.out
 
 source activate metawrap2-env
-#salmon index -k 21 -p 12 -t Annotated_Gene_Seqs.fa -i AGS_Sal_Ind
+sq_dir=/home-3/karoraw1@jhu.edu/scratch/MWMW/Data/KEGG_Annotations
+#salmon index -k 21 -p 12 -t $sq_dir/Annotated_Gene_Seqs.fa -i $sq_dir/AGS_Sal_Ind
 
 B_A_S=/home-3/karoraw1@jhu.edu/scratch/metaWRAP_Out/QC_Renamed
 
-mkdir -p QuantFiles
+mkdir -p $sq_dir/QuantFiles
 
 while read sample R1s R2s; do
-    salmon quant --meta -i AGS_Sal_Ind --libType IU -1 $B_A_S/$R1s -2 $B_A_S/$R2s -o QuantFiles/${sample}.quant;
+    salmon quant --meta -i $sq_dir/AGS_Sal_Ind --libType IU -1 $B_A_S/$R1s -2 $B_A_S/$R2s -o $sq_dir/QuantFiles/${sample}.quant;
 done < $B_A_S/samples_names.txt
 
-cd QuantFiles
-python summarize_salmon.py
-python concatenate_salmon.py
+#cd QuantFiles
+#python summarize_salmon.py
+#python concatenate_salmon.py
