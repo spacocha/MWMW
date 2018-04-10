bf52ddc2a0b2b9995e7c7ba4f9ecd61d05a8c55e
Sarah Preheim <sprehei1@jhu.edu>
2018-04-10 14:54:25 -0400

    Updated Blast search parameters

--- Scripts/iron_reducters.sh
+++ Scripts/iron_reducters.sh
@@ -3,20 +3,22 @@ module load blast
 db_db=../Data/KEGG_Annotations/Iron_Reducers
 out_dir=../Data/KEGG_Annotations/Iron_Reducers/match_dir
 mkdir $out_dir
-#makeblastdb -in $db_db/Rfer_all_proteins.fa -dbtype 'nucl' -out $db_db/Rfer_all_proteins 
+makeblastdb -in $db_db/Rfer_all_proteins.fa -dbtype 'nucl' -out $db_db/Rfer_all_proteins 
 
 ffn_dir=../Data/Bin_Fnas_Gffs_Ffns_and_Faas/bin_untranslated_genes
 for qq_ff in `ls $ffn_dir`; do
 echo $qq_ff;
-blastn -task megablast -num_threads 24 -db $db_db/Rfer_all_proteins -outfmt '6 qseqid qstart qend qlen sseqid staxids sstart send bitscore nident evalue length' -query $ffn_dir/$qq_ff -out $out_dir/${qq_ff}.hits -evalue 0.001;
+tblastx -num_threads 24 -db $db_db/Rfer_all_proteins -outfmt '6 qseqid qstart qend qlen sseqid staxids sstart send bitscore nident evalue length' -query $ffn_dir/$qq_ff -out $out_dir/${qq_ff}.hits -evalue 1E-150;
+#Add something here to process all of the results into a table
 done
 
-#makeblastdb -in $db_db/cytochrome_protein.fasta -dbtype 'prot' -out $db_db/cytochrome_protein 
+makeblastdb -in $db_db/cytochrome_protein.fasta -dbtype 'prot' -out $db_db/cytochrome_protein 
 
 
 faa_dir=../Data/Bin_Fnas_Gffs_Ffns_and_Faas/bin_translated_genes
 for qq_ff in `ls $faa_dir`; do
 echo $qq_ff;
-blastp -query $faa_dir/$qq_ff -num_threads 24 -db $db_db/cytochrome_protein -outfmt '6 qseqid qstart qend qlen sseqid staxids sstart send bitscore nident evalue length' -out $out_dir/${qq_ff}.hits -evalue 0.001;
+blastp -query $faa_dir/$qq_ff -num_threads 24 -db $db_db/cytochrome_protein -outfmt '6 qseqid qstart qend qlen sseqid staxids sstart send bitscore nident evalue length' -out $out_dir/${qq_ff}.hits -evalue 1E-150;
+#Add something here to process all the results into a table
 done
 
