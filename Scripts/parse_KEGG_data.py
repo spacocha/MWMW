dff8f061f808b77818404b2f2f6d2cd4fd80b2d3
Keith Arora-Williams <login@Keiths-MacBook-Air.local>
2018-04-13 18:49:01 -0400

    manual override and code simplification

--- Scripts/parse_KEGG_data.py
+++ Scripts/parse_KEGG_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sys
 from collections import Counter
-
+sys.argv = ['../Data/KEGG_Annotations']
 fasta_file = sys.argv[-1]+"/AllProteinsRenamed.faa"
 with open(fasta_file, "r") as ff_h:
     headers = [i[1:].split()[0] for i in ff_h.read().split("\n") if i != "" and i.startswith(">")]
@@ -87,17 +87,17 @@ for h in headers:
 master_df = pd.DataFrame(master_dicts)
 b1 = master_df.K_number_1.isnull()
 b2 = master_df.KO_Annot_1.isnull()
-clean_master = master_df[~(b1 & b2)]
-b3 = clean_master.K_number_1.isnull()
-b4 = clean_master.KO_Annot_1.isnull()
+unclean_master = master_df[~(b1 & b2)]
+b3 = unclean_master.K_number_1.isnull()
+b4 = unclean_master.KO_Annot_1.isnull()
 
-print "All annotation classification scores (n={})".format(clean_master.shape[0])
-print "\tMean: {}".format(clean_master.GHOSTX_score.mean())
-print "\tSTD: {}".format(clean_master.GHOSTX_score.std())
-print "\tMax: {}".format(clean_master.GHOSTX_score.max())
-print "\tMin: {}".format(clean_master.GHOSTX_score.min())
+print "All annotation classification scores (n={})".format(unclean_master.shape[0])
+print "\tMean: {}".format(unclean_master.GHOSTX_score.mean())
+print "\tSTD: {}".format(unclean_master.GHOSTX_score.std())
+print "\tMax: {}".format(unclean_master.GHOSTX_score.max())
+print "\tMin: {}".format(unclean_master.GHOSTX_score.min())
 
-just_K_nums = clean_master[~b3 & b4]
+just_K_nums = unclean_master[~b3 & b4]
 
 print "Taxonomy File (only) K-numbers (n={})".format(just_K_nums.shape[0])
 print "\tMean: {}".format(just_K_nums.GHOSTX_score.mean())
@@ -105,7 +105,7 @@ print "\tSTD: {}".format(just_K_nums.GHOSTX_score.std())
 print "\tMax: {}".format(just_K_nums.GHOSTX_score.max())
 print "\tMin: {}".format(just_K_nums.GHOSTX_score.min())
 
-just_KOs = clean_master[~b4 & b3]
+just_KOs = unclean_master[~b4 & b3]
 print "Annotation File (only) K-numbers (n={})".format(just_KOs.shape[0])
 
 ordered_cols = ["Protein_Name", "Bin_Name", "KO_Annot_1","KO_Annot_2", "KO_Annot_3",
@@ -115,9 +115,12 @@ ordered_cols = ["Protein_Name", "Bin_Name", "KO_Annot_1","KO_Annot_2", "KO_Annot
 print "Filtering out GHOSTX scores below 100, see code for citation link"
 # https://www.hindawi.com/journals/bmri/2016/8124636/
 
-clean_master = clean_master[clean_master.GHOSTX_score > 100]
+keeping_on_purpose = ["K03421", "K03422", "K00399", "K00400", "K00401", "K00402"]
+manual_override = unclean_master.K_number_1.isin(keeping_on_purpose)
+cleaner_master = unclean_master[(unclean_master.GHOSTX_score > 100) | manual_override ]
 
-clean_master = clean_master[ordered_cols]
+
+clean_master = cleaner_master[ordered_cols]
 clean_master.to_csv(sys.argv[-1]+"/Aggregated_Annotations.tsv", sep="\t", index=False)
 
 select_K_nums = ["K00362", "K00363", "K00368", "K00370", "K00371", "K00376", "K00394", 
@@ -125,30 +128,17 @@ select_K_nums = ["K00362", "K00363", "K00368", "K00370", "K00371", "K00376", "K0
                  "K02568", "K03385", "K04561", "K07218", "K08738", "K10535", "K10944", 
                  "K10945", "K10946", "K11180", "K11181", "K14028", "K15864", "K16255", 
                  "K16257", "K16258", "K16259", "K17222", "K17223", "K17224", "K17225", 
-                 "K17226", "K17227"]
+                 "K17226", "K17227", "K03421", "K03422", "K08928", "K08929", "K00855"]
 
-select_bools_1 = [clean_master.KO_Annot_1 == i for i in select_K_nums]
-select_bools_2 = [clean_master.KO_Annot_2 == i for i in select_K_nums]
-select_bools_3 = [clean_master.KO_Annot_3 == i for i in select_K_nums]
-select_bools_4 = [clean_master.K_number_1 == i for i in select_K_nums]
-select_bools_5 = [clean_master.K_number_2 == i for i in select_K_nums]
-select_bools_6 = [clean_master.K_number_3 == i for i in select_K_nums]
 
-select_bools = select_bools_1 + select_bools_2 + select_bools_3 + select_bools_4 + select_bools_5 + select_bools_6
 
-select_master = clean_master.copy()
-
-print "Select boolean filters length = {}".format(len(select_bools))
-
-for idx, sb in enumerate(select_bools):
-    select_master["Filter_{}".format(idx)] = sb
-
-print "Filter columns added equal {} - {}".format(select_master.shape[1],
-                                                  clean_master.shape[1])
-
-select_master = select_master[select_master.select_dtypes([bool]).any(1)]
-
-select_master.drop([i for i in list(select_master.columns) if "Filter_" in i], axis=1, inplace=True)
+select_bools_1 = clean_master.KO_Annot_1.isin(select_K_nums)
+select_bools_2 = clean_master.KO_Annot_2.isin(select_K_nums)
+select_bools_3 = clean_master.KO_Annot_3.isin(select_K_nums)
+select_bools_4 = clean_master.K_number_1.isin(select_K_nums)
+select_bools_5 = clean_master.K_number_2.isin(select_K_nums)
+select_bools_6 = clean_master.K_number_3.isin(select_K_nums)
+select_master = clean_master[select_bools_1 | select_bools_2 | select_bools_3 | select_bools_4 | select_bools_5 | select_bools_6]
 
 print "Proteins annotated as select classes (n={})".format(select_master.shape[0])
 print "\tMean: {}".format(select_master.GHOSTX_score.mean())
@@ -166,7 +156,8 @@ for a_bin in bin_numbers:
     for k_num in select_K_nums:
         k_counter[k_num] = 0
     k_counts_by_bin.append(k_counter)
-        
+
+
 for idx, this_bin in enumerate(bin_numbers):    
     bin_df = select_master[select_master.Bin_Name == this_bin]
     countdown = countdown - bin_df.shape[0]
